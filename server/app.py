9c90684e282359d0bf8a22b89b97730a200dd9ef
ituman <van.tum@mail.ru>
2021-03-15 01:28:50 +0300

    Добавил параметр для фильтрации по сайту

--- server/app.py
+++ server/app.py
@@ -15,10 +15,16 @@ def search_product():
     results_size = int(request.args.get('size', 15))
     if not query:
         return jsonify({'status': 'error', 'message': 'EMPTY_QUERY'})
+    companies_ids = request.args.get('apis', '')
+    if companies_ids:
+        companies_ids = list(map(lambda company_id: int(company_id), companies_ids.split(',')))
+        companies_to_search = filter(lambda company: company.get_id() in companies_ids, COMPANIES_LIST)
+    else:
+        companies_to_search = COMPANIES_LIST
 
     searched_products = []
     search_params = SearchParams(query=query, size=results_size)
-    for company in COMPANIES_LIST:
+    for company in companies_to_search:
         searched_products.extend(
             company.search(search_params)
         )
